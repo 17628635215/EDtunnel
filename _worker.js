45368f2e2dd1200beefed711cc64b421fd0cb481
Iven <1440872401@qq.com>
2024-01-22 17:01:28 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
-const proxyIPs = ['23.162.136.169', 'cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org'];
+const proxyIPs = ['fbi.gov', 'www.udemy.com', 'www.glassdoor.com', 'www.gov.se', 'www.shopify.com', 'cdn.anycast.eu.org', 'download.yunzhongzhuan.com', 'whatismyipaddress.com'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
